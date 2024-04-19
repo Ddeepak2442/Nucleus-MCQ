1d2f5154739ef2390c77c246b29f963db01ca63a
KATARI REVATHI <katari.revathi@gmail.com>
2024-04-19 18:51:33 +0530

    mcq resolved

--- MCQS/urls.py
+++ MCQS/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView,SubjectView, TopicListView
+from .views import HomeView,SubjectView, TopicListView,mcq_quiz
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='home_new'),
     path('subjects/', SubjectView.as_view(), name='subject_list'),
     path('subjects/<str:subject_slug>/topics/', TopicListView.as_view(), name='topic_list'),
+    path('topics/<str:topic_slug>/<int:question_num>/', mcq_quiz, name='mcq_quiz'),
 ] 
 
--- MCQS/views.py
+++ MCQS/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView,TemplateView
-from . models import Subject,Topic
+from . models import Subject,Topic,Question
 from django.db.models import Count
 
 from django.http import HttpRequest
@@ -51,3 +51,33 @@ class TopicListView(ListView):
             subject = get_object_or_404(Subject, slug=subject_slug)
             context['subject'] = subject
         return context
+    
+def mcq_quiz(request, topic_slug, question_num):
+    print("Debugging - Entered mcq_quiz view")
+    
+    topic = get_object_or_404(Topic, slug=topic_slug)
+    print("Debugging - Retrieved topic:", topic)
+
+    questions = Question.objects.filter(sub_topic_name__topic_name=topic)
+    print("Debugging - Retrieved questions:", questions)
+    
+    if not questions.exists() or question_num > len(questions) or question_num < 1:
+        print("Debugging - Redirecting due to invalid question number or empty question queryset")
+        return redirect('topic_list', subject_slug=topic.subject.slug)  # Redirect to topic list or some other page
+
+    current_question = questions[question_num - 1]
+    print("Debugging - Retrieved current question:", current_question)
+
+    opt_values = current_question.opt_values.split(';') if current_question.opt_values else []
+    print("Debugging - Retrieved options:", opt_values)
+
+    context = {
+        'topic': topic,
+        'current_question': current_question,
+        'opt_values': opt_values,
+        'question_num': question_num,
+        'total_questions': len(questions),
+    }
+    
+    print("Debugging - Rendering mcq.html template with context:", context)
+    return render(request, 'mcq/mcq.html', context)
