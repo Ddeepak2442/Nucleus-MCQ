9c310032249256f5ce9923706339be796956a4f8
KATARI REVATHI <katari.revathi@gmail.com>
2024-05-22 13:40:48 +0530

    message

--- nucleus/settings.py
+++ nucleus/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 
 # Application definition
